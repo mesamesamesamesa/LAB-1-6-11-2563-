a285ab92bb71d2d34871e27074e840023040ec67
mesamesamesamesa <73683412+mesamesamesamesa@users.noreply.github.com>
2020-11-06 13:33:59 +0700

    Add files via upload

---
+++ test1_1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <img src="img/creative/creative8.jpg" alt="" height="100%" width="100%">
+                <h1>In town</h1>
+                <p1>when i go home</p1>
+            </div>
+            <div class="col-lg-4">
+                <img src="img/staffs/staff9.jpg" alt="" height="100%" width="100%">
+                <h1>my friend</h1>
+                <p1>his name is god</p1>
+            </div>
+            <div class="col-lg-4">
+                <img src="img/content/office3.jpg" alt="" height="100%" width="100%">
+                <h1>my office</h1>
+                <p1>so boring</p1>
+            </div>
+        </div>
